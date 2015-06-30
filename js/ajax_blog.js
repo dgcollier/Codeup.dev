7b51f24a419cdc588732cfcff277729edc6f5369
dgcollier <david@gcollier.me>
2015-06-30 16:00:48 -0500

    ajax blog, added pics to site index

---
+++ js/ajax_blog.js
@@ -0,0 +1,65 @@
+"use strict";
+    (function() {
+
+        addPost();
+
+        function addPost() {
+        
+            var ajaxRequest = $.ajax("/data/blog.json");
+
+            ajaxRequest.always(function () {
+                console.log("Fetching posts...");
+            });
+
+            ajaxRequest.fail(function () {
+                console.log("No posts found.");
+                alert("No posts found");
+            });
+
+            var blogPosts;
+            ajaxRequest.done(function (data) {
+
+                for (var i = 0; i < data.length; i++) {
+                    blogPosts = $("#posts");
+                    blogPosts.append("<h3>" + data[i].title + "</h3>");
+                    blogPosts.append("<p>" + data[i].content + "</p>");
+                    blogPosts.append("<h4 class='dateHeader'>" + "Date:" + "</h4>");
+                    blogPosts.append("<span class='date'>" + data[i].date + "</span>");
+                    blogPosts.append("<h5 class='tags'>" + "Tags:" + "</h5>")
+                    blogPosts.append("<span class='categories'>" + data[i].categories + "</span>");
+                    blogPosts.append("<br><button id='removePost' class='btn btn-default btn-xs'>" + "Delete This Post" + "</button>");
+                    $("#posts").append(blogPosts);
+                }
+                console.log("Blog posts found.");
+            });
+        }
+
+        $("#addPost").click(function () {
+
+        });
+    })();
+
+    $(document).ready(function (){
+
+        $("#addPost").click(function () {
+            $("#addPostForm").toggleClass("invisible");
+
+            if($(this).text() === "Add New Post") {
+                $(this).text("Cancel");
+            } else {
+                $(this).text("Add New Post");
+            }
+        });
+
+        $("#removePost").click(function () {
+
+        })
+    });
+
+    // Don't forget to try .split() on your tags to separate btw commas
+
+    // EXTRA CHALLENGES
+    // attach event listener for new post form
+    // attach event listener for remove post button
+
+    // local storage
