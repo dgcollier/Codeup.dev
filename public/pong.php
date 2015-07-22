f305b9d3a76883dfab8e69621a876deb08b49416
dgcollier <david@gcollier.me>
2015-07-22 09:27:17 -0500

    Completed exercise.

---
+++ public/pong.php
@@ -0,0 +1,95 @@
+<?php
+
+    function pageController()
+    {
+        $data = [];
+ 
+        if (empty($_GET['score'])) {
+            $score = 0;
+            $hitBtn = 'START';
+        } else {
+            $score =  $_GET['score'];
+        }
+
+        if (empty($_GET['hit'])) {
+            $score = 0;
+            $hitBtn = 'START';
+            $text = "Let's play!";
+        } else {
+            $score++;
+            $hitBtn = 'HIT';
+            $text = 'Hit it again!';
+        }
+        
+        $data['score'] = $score;
+        $data['hitBtn'] = $hitBtn;
+        $data['text'] = $text;  
+
+        return $data;
+    }
+
+    extract(pageController());
+    // var_dump($_GET['hit']);
+?>
+
+<!DOCTYPE>
+<html>
+<head>
+    <title>PONG</title>
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+    <style type="text/css">
+        body {
+            text-align: center;
+        }
+
+        button {
+            width: 150px;
+
+        }
+
+        #quit {
+            position: fixed;
+            top: 400px;
+            left: 640px;
+        }
+
+        #score {
+            position: fixed;
+            top: 475px;
+            left: 640px;  
+        }
+
+        #ping {
+            background-color: #FFFF00;
+            border-radius: 100%;
+            width: 75px;
+            height: 75px;
+            margin-right: 200px;
+            margin-top: 50px;
+            float: right;
+            border: solid #A09D8E 2px;
+        }
+
+    </style>
+</head>
+<body>
+    <h1>PONG</h1>
+    <a href="http://codeup.dev/ping.php?hit=<?=true?>&score=<?= $score; ?>">
+        <button id="hitBtn" class="btn btn-lg"><?= $hitBtn; ?></button>
+    </a>
+
+    <a href="http://codeup.dev/ping.php?hit=<?=false?>&score=<?= $score; ?>">
+        <button id="miss" class="btn btn-lg">MISS</button>  
+    </a>
+    <h4><?= $text ?></h4>
+
+    <div id="ping"></div>
+
+    <a href="http://codeup.dev/pong.php">
+        <button id="quit" class="btn btn-lg">Quit</button>  
+    </a>
+
+    <button id="score" class="btn btn-lg">Score: <?= $score; ?></button>
+
+</body>
+</html>
