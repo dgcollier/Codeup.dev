1207d1766577ebd84d046062be7fa735d2b044d9
dgcollier <david@gcollier.me>
2015-07-22 10:02:07 -0500

    File created with login form.

---
+++ public/login.php
@@ -0,0 +1,23 @@
+<?php  
+    var_dump($_POST);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+    <form method="POST" action=>
+        <label>Username</label>
+        <input type="text" name="username"><br>
+        <label>Password</label>
+        <input type="password" name="password"><br>
+        <input type="submit"> 
+
+    </form>
+
+</body>
+</html>
