5db500f3fc73311aa6095c021b7bca23945f6512
dgcollier <david@gcollier.me>
2015-08-03 12:27:36 -0500

    Modified query to load four parks at a time.

--- public/national_parks.php
+++ public/national_parks.php
@@ -3,7 +3,31 @@
     require_once "../parks_config.php";
     require_once "../db_connect.php";
 
-    $stmt = $dbc->query('SELECT name, location, date_established, area_in_acres FROM national_parks');
+
+    $parksPerPage = 4;
+    $stmt = $dbc->query('SELECT COUNT(*) FROM national_parks');
+    $totalParks = $stmt->fetchColumn();
+    $lastPage = ceil($totalParks / $parksPerPage);
+    print_r($totalParks . PHP_EOL);
+    print_r($lastPage . PHP_EOL);
+
+    if (isset($_GET['page'])) {
+        $page = $_GET['page'];
+    } else {
+        $page = 1;
+    }
+
+    $page = intval($page);
+
+    if ($page > $lastPage) {
+        $page = $lastPage;
+    }
+
+    if ($page < 1) {
+        $page = 1;
+    }
+
+    $stmt = $dbc->query('SELECT name, location, date_established, area_in_acres FROM national_parks LIMIT '. ($page - 1) * $parksPerPage . ', ' . $parksPerPage);
     $parks = $stmt->fetchAll(PDO::FETCH_ASSOC);
     // var_dump($parks);
 
@@ -30,9 +54,13 @@
             <td><?= $park['location'] ?></td>
             <td><?= $park['date_established'] ?></td>
             <td><?= $park['area_in_acres'] ?></td>
-        <? endforeach; ?>
         </tr>
+        <? endforeach; ?>
     </table>
+
+    <a href="?page=1">1</a>
+    <a href="?page=2">2</a>
+    <a href="?page=3">3</a>
 
 
 </body>
