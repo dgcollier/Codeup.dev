9f2d2aed93f8f471bd329769e0cbcd1a728e8d81
dgcollier <david@gcollier.me>
2015-08-04 11:59:41 -0500

    Added unique index.

--- park_migration.php
+++ park_migration.php
@@ -9,13 +9,14 @@
 
     $dbc->exec($dropIf);
 
-    $addTable = "CREATE TABLE national_parks (
+    $addTable = 'CREATE TABLE national_parks (
         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
         name VARCHAR(20) NOT NULL,
         location VARCHAR(30) NOT NULL,
         date_established DATE NOT NULL,
         area_in_acres DOUBLE(10,2) NOT NULL,
         description TEXT,
-        PRIMARY KEY (id))";
+        PRIMARY KEY (id),
+        UNIQUE KEY "park_state_unq" ("name","location"))';
 
     $dbc->exec($addTable);
